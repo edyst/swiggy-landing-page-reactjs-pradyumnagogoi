c166912c30bf6ff62e99e5187e3f553833232379
Pradyumna <pradyumnagogoi.cse19@chitkarauniversity.edu.in>
2021-09-21 08:09:28 +0530

    final init

---
+++ swiggy-app/src/App.js
@@ -0,0 +1,19 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import Features from './Features Section/Features';
+import Footer from './Footer Section/Footer';
+import JumboSection from './Jumbo Section/JumboSection';
+import Resturent from './Resturent in your pocket/Resturent'
+
+function App() {
+  return (
+    <div className="App">
+    <JumboSection />
+    <Features />
+    <Resturent />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
